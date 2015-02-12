6d39a92423237a49f18da983ebecf16d6bbd36e6
snowman2 <alan.d.snow@byu.edu>
2015-02-12 23:39:56 +0000

    Fixed file path in .sh file

--- rapid_process.sh
+++ rapid_process.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/home/sgeadmin/work/scripts/rapid/rapid_process_async_ubuntu.py 1> /mnt/sgeadmin/logs/rapid_$(date +%y%m%d%H%M%S).log 2>&1
+/home/sgeadmin/work/scripts/erfp_data_process_ubuntu_aws/rapid_process_async_ubuntu.py 1> /mnt/sgeadmin/logs/rapid_$(date +%y%m%d%H%M%S).log 2>&1
